c6ede661fe1537e2be46ddd5b57651f1197a0a44
Matt Ramirez <mbramire@live.com>
2013-06-26 21:19:39 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapwater::Application.config.secret_token = 'cf1c59228c0085c5d9d207c53845fe91c02410420f6aa6bf001a8f15606e28dec9cd99f36d22d04f20d2d35594fd99a7bec7112f529b6d5dfacc93b3d453a3d3'
